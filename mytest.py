f3bd549247580df02d11f8f092a9dca2f1582463
agnieszka.kirilow <agnieszka.kirilow@gmail.com>
2023-10-09 20:17:49 +0200

    add my_test4

--- mytest.py
+++ mytest.py
@@ -2,7 +2,10 @@ def my_test():
     assert 1 + 2 == 3 
 
 def my_test2():
-    assert 1 * 7 == 7   
+    assert 1 * 7 == 7
+
+def my_test4():
+    assert 3 * 8 ==24   
 
 if __name__ == "__main__":
     my_test()
