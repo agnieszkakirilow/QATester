4d5a65a0c6f54a4ab8a4842cacb81f246e86befb
agnieszka.kirilow <agnieszka.kirilow@gmail.com>
2023-11-06 07:42:05 +0100

    move repetitive actions to configtest.py

--- tests/github/api/test_api.py
+++ tests/github/api/test_api.py
@@ -1,23 +1,23 @@
-import requests
-from src.config.config import Config
-from src.applications.github.api.github_api import GitHubAPI
+from confitest import git_hub_api_app
 
-def test_search_for_existing_repo():
-    
-    github_api_client = GitHubAPI
+def test_search_for_existing_repo(git_hub_api_app):
+
+    # test steps
     existing_repo_name = 'sergii'
-    repos = github_api_client.search_repos(existing_repo_name)
+    repos = git_hub_api_app.search_repos(existing_repo_name)
 
     assert repos['total_count'] != 0
 
-def test_search_for_non_existing_repo():
     
-    github_api_client = GitHubAPI
+def test_search_for_non_existing_repo(git_hub_api_app):
+    
+    
     non_existing_repo_name = 'lkjkjhg'
-    repos = github_api_client.search_repos(non_existing_repo_name)
+    repos = git_hub_api_app.search_repos(non_existing_repo_name)
 
     assert repos['total_count'] == 0
 
 
 # -s
+
 
